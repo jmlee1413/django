acc97a42683d5a9d50059112b409bb96cfc26f68
ljm <jmlee1413@naver.com>
2020-05-25 01:51:59 +0900

    init

---
+++ elections/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Candidate, Poll, Choice
+# Register your models here.
+admin.site.register(Candidate)
+admin.site.register(Poll)
+admin.site.register(Choice)
